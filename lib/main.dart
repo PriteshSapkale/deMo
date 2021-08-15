fd2372adbaede2e610a0ee782df3da6a3bd72350
shiba_kar <shibaprasadkar50@gmail.com>
2021-08-15 20:54:59 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,45 @@ class StaggeredGridExample extends StatefulWidget {
 class _StaggeredGridExampleState extends State<StaggeredGridExample> {
   List<DisplayModel> images = [
     DisplayModel(
-        image:
-            'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
-        label: 'Cotton Dress'),
+      image:
+          'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
+      label: 'Cotton Dress',
+    ),
     DisplayModel(
-        image:
-            'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
-        label: 'Cotton Dress'),
+      image:
+          'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
+      label: 'Cotton Dress',
+    ),
     DisplayModel(
-        image:
-            'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
-        label: 'Cotton Dress'),
+      image:
+          'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
+      label: 'Cotton Dress',
+    ),
     DisplayModel(
-        image:
-            'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
-        label: 'Cotton Dress'),
+      image:
+          'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
+      label: 'Cotton Dress',
+    ),
     DisplayModel(
-        image:
-            'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
-        label: 'Cotton Dress'),
+      image:
+          'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
+      label: 'Cotton Dress',
+    ),
     DisplayModel(
-        image:
-            'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
-        label: 'Cotton Dress'),
+      image:
+          'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
+      label: 'Cotton Dress',
+    ),
     DisplayModel(
-        image:
-            'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
-        label: 'Cotton Dress'),
+      image:
+          'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
+      label: 'Cotton Dress',
+    ),
     DisplayModel(
-        image:
-            'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
-        label: 'Cotton Dress'),
+      image:
+          'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
+      label: 'Cotton Dress',
+    ),
   ];
 
   var x = [
@@ -78,56 +86,29 @@ class _StaggeredGridExampleState extends State<StaggeredGridExample> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80'),
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  Text(
-                    'Hi,Victoria',
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                  SizedBox(width: 120),
-                  ElevatedButton(
-                      onPressed: () => Navigator.push(
-                          context,
-                          (MaterialPageRoute(
-                              builder: (context) => SecondScreen()))),
-                      child: Icon(Icons.shopping_bag))
-                ],
-              ),
-              Column(
-                children: [
-                  StaggeredGridView.countBuilder(
-                    crossAxisCount: 4,
-                    itemCount: images.length,
-                    itemBuilder: (BuildContext context, int index) => Card(
-                      child: Column(
-                        children: <Widget>[
-                          Image.network(images.image),
-                          Text(
-                            images.label,
-                          ),
-                        ],
-                      ),
-                    ),
-                    staggeredTileBuilder: (int index) =>
-                        new StaggeredTile.fit(2),
-                    mainAxisSpacing: 4.0,
-                    crossAxisSpacing: 4.0,
-                  ),
-                ],
-              ),
-            ],
+        appBar: AppBar(
+          elevation: 0.0,
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80'),
           ),
+        ),
+        body: StaggeredGridView.countBuilder(
+          crossAxisCount: 4,
+          itemCount: images.length,
+          itemBuilder: (BuildContext context, int index) => Card(
+            child: Column(
+              children: <Widget>[
+                Image.network(images[index].image),
+                Text(
+                  images[index].label,
+                ),
+              ],
+            ),
+          ),
+          staggeredTileBuilder: (int index) => new StaggeredTile.fit(2),
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
         ),
       ),
     );
